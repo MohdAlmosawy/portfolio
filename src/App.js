353ce8e227aca6637a10dc8e5c3ab8a3edcf0ab1
sebrahim <mohd.s.aqeel@gmail.com>
2024-08-22 12:29:34 +0300

    Reorganized project structure into component-specific folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // src/App.js
 import React, { useState } from 'react';
-import SplashScreen from './SplashScreen';
-import HomePage from './HomePage';
 import './App.css';
+
+// Updated import paths
+import SplashScreen from './components/SplashScreen/SplashScreen';
+import HomePage from './components/HomePage/HomePage';
+import ComingSoon from './components/ComingSoon/ComingSoon';
 
 function App() {
   const [showSplash, setShowSplash] = useState(true);
